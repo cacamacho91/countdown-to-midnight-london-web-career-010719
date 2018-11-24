e086fc6074d598fabad25c3989d2bf1cd0f40251
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-24 22:49:32 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(countdown_number)
+  while countdown_number > 0
+    puts "#{countdown_number} SECOND(S)!"
+    countdown_number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(countdown_number)
+  while countdown_number > 0
+    puts "#{countdown_number} SECOND(S)!"
+    countdown_number -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
